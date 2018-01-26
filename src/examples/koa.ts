63c7e4e5c2555df9ffd0e6713b9485722ebc27f5
Lee Henson <lee.m.henson@gmail.com>
2018-01-26 09:53:28 +0000

    remove koa-router from example

--- src/examples/koa.ts
+++ src/examples/koa.ts
@@ -1,5 +1,4 @@
 import * as Koa from 'koa'
-import * as Router from 'koa-router'
 import { Status, StatusOpen, ResponseEnded } from '..'
 import { status, closeHeaders, send, MiddlewareTask } from '../../src/MiddlewareTask'
 import { KoaConn } from '../adapters/koa'
@@ -9,11 +8,10 @@ const hello = status(Status.OK)
   .ichain(() => send('Hello hyper-ts on koa!'))
 
 const app = new Koa()
-const router = new Router()
 
-const toRequestHandler = (task: MiddlewareTask<StatusOpen, ResponseEnded, void>): Router.IMiddleware => ctx =>
+const toRequestHandler = (task: MiddlewareTask<StatusOpen, ResponseEnded, void>): Koa.Middleware => ctx =>
   task.eval(new KoaConn(ctx)).run()
 
-app.use(router.get('/', toRequestHandler(hello)).routes()).listen(3000, () => {
+app.use(toRequestHandler(hello)).listen(3000, () => {
   console.log('Koa listening on port 3000. Use: GET /')
 })
