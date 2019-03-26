5c28f95a10c20c389eb19b6a7be7c51adedcda05
gcanti <giulio.canti@gmail.com>
2019-03-26 10:04:26 +0100

    rename Conn to Connection

--- src/koa.ts
+++ src/koa.ts
@@ -1,14 +1,14 @@
 import { identity } from 'fp-ts/lib/function'
 import { Task } from 'fp-ts/lib/Task'
 import * as koa from 'koa'
-import { Conn, CookieOptions, Middleware, ResponseEnded, Status, StatusOpen } from '.'
+import { Connection, CookieOptions, Middleware, ResponseEnded, Status, StatusOpen } from '.'
 import { IO, io } from 'fp-ts/lib/IO'
 
-export class KoaConn<S> implements Conn<S> {
+export class KoaConnection<S> implements Connection<S> {
   readonly _S!: S
   constructor(readonly context: koa.Context, readonly action: IO<unknown> = io.of(undefined)) {}
-  chain<T>(thunk: () => void): KoaConn<T> {
-    return new KoaConn<T>(this.context, this.action.chain(() => new IO(thunk)))
+  chain<T>(thunk: () => void): KoaConnection<T> {
+    return new KoaConnection<T>(this.context, this.action.chain(() => new IO(thunk)))
   }
   getBody() {
     return this.context.request.body
@@ -43,17 +43,17 @@ export class KoaConn<S> implements Conn<S> {
   setBody<T>(body: unknown) {
     this.action.run()
     this.context.body = body
-    return new KoaConn<T>(this.context)
+    return new KoaConnection<T>(this.context)
   }
   endResponse<T>() {
     this.action.run()
     this.context.response.res.end()
-    return new KoaConn<T>(this.context)
+    return new KoaConnection<T>(this.context)
   }
 }
 
-export function toRequestHandler(f: (c: KoaConn<StatusOpen>) => Task<void>): koa.Middleware {
-  return ctx => f(new KoaConn<StatusOpen>(ctx)).run()
+export function toRequestHandler(f: (c: KoaConnection<StatusOpen>) => Task<void>): koa.Middleware {
+  return ctx => f(new KoaConnection<StatusOpen>(ctx)).run()
 }
 
 export function fromMiddleware(middleware: Middleware<StatusOpen, ResponseEnded, never, void>): koa.Middleware {
