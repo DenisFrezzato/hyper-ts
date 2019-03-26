30be67bb8ad5efc08bd8ee645252278afa097789
gcanti <giulio.canti@gmail.com>
2019-03-26 11:00:28 +0100

    make json function safer

--- dtslint/ts3.1/index.ts
+++ dtslint/ts3.1/index.ts
@@ -1,10 +1,23 @@
-import { header, status, Status } from '../../src'
+import { header, status, Status, json, Middleware, BodyOpen, HeadersOpen } from '../../src'
 
 // $ExpectError
-status(1000)
+const m1 = status(1000)
 
-status(Status.OK)
+const m2 = status(Status.OK)
   .closeHeaders()
   .send('Hello hyper-ts!')
   // $ExpectError
   .ichain(() => header('field', 'value'))
+
+//
+// json
+//
+
+const m3 = json({ a: 1 })
+
+declare const m4: Middleware<HeadersOpen, BodyOpen, never, void>
+
+// $ExpectError
+const m5 = m4.ichain(() => m3)
+// $ExpectError
+const m6 = json({ a: new Date() })
