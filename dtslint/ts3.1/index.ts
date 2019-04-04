fa41897b7c207bebf25f7452fe905309b4e7b576
gcanti <giulio.canti@gmail.com>
2019-04-04 16:58:01 +0200

    fix dtslint

--- dtslint/ts3.1/index.ts
+++ dtslint/ts3.1/index.ts
@@ -8,16 +8,3 @@ const m2 = status(Status.OK)
   .send('Hello hyper-ts!')
   // $ExpectError
   .ichain(() => header('field', 'value'))
-
-//
-// json
-//
-
-const m3 = json({ a: 1 })
-
-declare const m4: Middleware<HeadersOpen, BodyOpen, never, void>
-
-// $ExpectError
-const m5 = m4.ichain(() => m3)
-// $ExpectError
-const m6 = json({ a: new Date() })
